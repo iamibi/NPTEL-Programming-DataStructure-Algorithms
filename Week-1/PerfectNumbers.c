67004df0457fb9ef1480d7c46e44e06595a336ac
Syed Mohammad Ibrahim <sumiibi@gmail.com>
2016-01-18 18:12:44 +0530

    Perfect Numbers

--- Week-1/PerfectNumbers.c
+++ Week-1/PerfectNumbers.c
@@ -1,3 +1,8 @@
+/*
+ * This program checks whether the number is perfect or not.
+ * A number is said to be perfect if its subsiquent divisor would sum up to the given number
+*/
+
 #include <stdio.h>
 
 int main()
@@ -5,13 +10,17 @@ int main()
     long int x, i = 1, res = 0;
     
     scanf ("%ld", &x);
+    
+    //Start i from 1 to x
     while (i < x)
     {
+        //If i divides x completely then it is a divisor for consideration
         if (x % i == 0)
-            res = res + i;
+            res = res + i;      //Add it to the resulting sum
         i++;
     }
     
+    //If the resulting sum is equal to the original number, then it is perfect
     if (res == x)
         printf ("yes");
     else
